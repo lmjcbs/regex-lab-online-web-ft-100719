84ba9e1e1b2265d984ab1cf83b7573e89b9551fd
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-16 15:35:54 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,6 @@
 def starts_with_a_vowel?(word)
-  word.scan(/\b[aeiou]/) ? true : false
+  result = word.scan(/\b[aeiou]/)
+  puts result
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
