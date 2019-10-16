bb857c8f06931ecb8325bab2e7b8b5979ce56077
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-16 16:00:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  result = text.scan(/[A-Z].*[@"_""'a:;%^&*~`bc!@#.,?"]/)
+  result = text.scan(/\A[A-Z].*[@"_""'a:;%^&*~`bc!@#.,?"]\z/)
   result == [] ? false : true
 end
 
