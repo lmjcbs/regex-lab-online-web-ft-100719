89176a2e19a3d83b41a3a6bac7917ec8b67604bb
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-16 15:39:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,5 @@
 def starts_with_a_vowel?(word)
-  result = word.scan(/\b[aeiouAEIOU]\w*/)
-  puts result
+  word.scan(/\b[aeiouAEIOU]\w*/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
